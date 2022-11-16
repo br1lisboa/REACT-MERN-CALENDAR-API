529fef934837b0cc7a76919769b8d3101581e3ef
br1lisboa <brlisbo@gmail.com>
2022-11-16 17:16:31 -0300

    CRUD EVENT OK

--- controllers/events.js
+++ controllers/events.js
@@ -3,11 +3,15 @@ const Evento = require('../models/Evento')
 
 
 //* OBTENER EVENTOS
-const obtenerEventos = (req, res = response) => {
+const obtenerEventos = async (req, res = response) => {
+
+    const eventos = await Evento.find()
+        .populate('user', 'name')
 
     res.json({
         ok: true,
-        msg: 'Eventos obtenidos'
+        msg: 'Eventos obtenidos',
+        eventos
     })
 
 }
@@ -17,7 +21,6 @@ const obtenerEventos = (req, res = response) => {
 const crearEvento = async (req = request, res = response) => {
 
     const evento = new Evento(req.body)
-    console.log(evento)
 
     try {
 
@@ -38,34 +41,92 @@ const crearEvento = async (req = request, res = response) => {
         })
     }
 
-
 }
 
 //* ACTUALIZAR EVENTO
-const modificarEventos = (req, res = response) => {
+const modificarEventos = async (req, res = response) => {
 
-    const body = req.body
+    /*     const body = req.body */
     const { id } = req.params
+    /* console.log(id) */
 
-    res.json({
-        ok: true,
-        msg: 'Evento modificado',
-        body,
-        id
-    })
+    try {
+
+        const evento = await Evento.findById(id)
+
+        if (!evento) {
+            res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese ID'
+            })
+        }
+
+        if (evento.user.toString() !== req.uid) {
+            res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegios para editar este evento'
+            })
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: req.uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate(id, nuevoEvento, { new: true }) // Este ultimo parametro nos refrezca la peticion
+
+        res.json({
+            ok: true,
+            msg: 'Evento modificado',
+            evento: eventoActualizado
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al actualizar el evento, hable con el administrador'
+        })
+    }
 
 }
 
 //* ELIMINAR EVENTO
-const eliminarEvento = (req = request, res = response) => {
+const eliminarEvento = async (req = request, res = response) => {
 
     const { id } = req.params
 
-    res.json({
-        ok: true,
-        msg: 'Evento eliminado',
-        id
-    })
+    try {
+
+        const eventoEliminado = await Evento.findByIdAndDelete(id)
+
+        if (!eventoEliminado) {
+            res.status(404).json({
+                ok: false,
+                msg: 'No existe evento con ese ID para eliminar'
+            })
+        }
+
+        if (eventoEliminado.user.toString() !== req.uid) {
+            res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegios para eliminar este evento'
+            })
+        }
+
+        res.json({
+            ok: true,
+            msg: 'Evento eliminado',
+            evento: eventoEliminado
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Fallo al eliminar el evento, hable con el administrador'
+        })
+    }
 
 }
 
--- routes/events.js
+++ routes/events.js
@@ -27,7 +27,12 @@ router.post('/', [
 ], crearEvento)
 
 //* Actualizar evento
-router.put('/:id', modificarEventos)
+router.put('/:id', [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'La fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'La fecha de finalizacion es obligatoria').custom(isDate),
+    validarCampos
+], modificarEventos)
 
 //* Borrar evento
 router.delete('/:id', eliminarEvento)
